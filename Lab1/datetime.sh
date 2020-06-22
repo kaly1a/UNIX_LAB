9b39ca61b37369279ff72be8374069e12ab3a291
Kalyani Nayak <60696076+kaly1a@users.noreply.github.com>
2020-06-22 09:12:35 +0530

    Create datetime.sh

---
+++ Lab1/datetime.sh
@@ -0,0 +1,16 @@
+/*
+ 
+ Program: Write Script to see current date, time, username, and current directory 
+ 
+ 
+*/
+
+echo "Name: Kalyani Nayak"
+echo "Regno: 1947228"
+
+echo "Current date, time, username and dir"
+echo "Hello, $LOGNAME"
+echo "Current date: `date`"
+echo "User: `who i am`"
+echo "Current directory: `pwd`"
+echo "Hostname: `HOSTNAME`"
